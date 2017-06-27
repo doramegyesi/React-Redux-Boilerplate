b7dd8f179a3d3fc750e391dc1ac19071d571b308
Dora Megyesi <dora.megyesi@gmail.com>
2017-06-27 11:14:25 +0200

    allReducers created

--- dev/js/index.js
+++ dev/js/index.js
@@ -2,8 +2,9 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from "react-dom";
 import {createStore} from "redux"; //can be used as a function
+import allReducers from "./reducers"
 
-const store = createStore(); //all the data of the app
+const store = createStore(allReducers); //all the data of the app
 
 ReactDOM.render(
     <h1>Hey now!</h1>,
