6fa29141213a5c30aab666c891617dfa40df7efc
Dora Megyesi <dora.megyesi@gmail.com>
2017-06-27 10:26:57 +0200

    users reducer

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,6 +1,9 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from "react-dom";
+import {createStore} from "redux"; //can be used as a function
+
+const store = createStore(); //all the data of the app
 
 ReactDOM.render(
     <h1>Hey now!</h1>,
---
+++ dev/reducers/reducer_users.js
@@ -0,0 +1,28 @@
+export default function() {
+    return [
+        {
+            id: 1,
+            first: "Janet",
+            last: "Snakehole";
+            age: 101,
+            description: "A very rich widow with a terrible secret.",
+            thumbnail: "https://s-media-cache-ak0.pinimg.com/736x/c1/fe/f8/c1fef80d5ea8716200d0c88fe8a1694f.jpg"
+        },
+        {
+            id: 2,
+            first: "Dev",
+            last: "Shah";
+            age: 33,
+            description: "Little bud.",
+            thumbnail: "http://cdn.worldscreen.com.tw/uploadfile/201512/goods_009837_164501.jpg"
+        },
+        {
+            id: 3,
+            first: "Jared",
+            last: "Dunn";
+            age: 35,
+            description: "The most awkward and adorable dude ever.",
+            thumbnail: "http://img.huffingtonpost.com/asset/scalefit_720_noupscale/571a0947160000e90031c70b.jpeg"
+        }
+    ]
+}
